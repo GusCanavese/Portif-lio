e14aab16e93a096c99b38090f7226f2c1c52fd80
Gustavo <bicalhogustavo866@gmail.com>
2024-12-09 01:28:10 -0300

    nova e penultima seção, correção da navbar e adicionando smooth scroll

--- js.js
+++ js.js
@@ -110,7 +110,76 @@ particlesJS("particles-js", {
     retina_detect: true
 });
 
-stats.domElement.style.position = 'relative';
-stats.domElement.style.left = '0px';
-stats.domElement.style.top = '0px';
-document.body.appendChild(stats.domElement);
+const menuLinks = document.querySelectorAll('.letrasOpcao a[href^="#"]')
+console.log(menuLinks);
+
+function getDistanceFromTheTop(element){
+    const id = element.getAttribute("href");
+    return document.querySelector(id).offsetTop;
+}
+
+function scrollToSection(event){
+    event.preventDefault();
+    const distanceFromTheTop = getDistanceFromTheTop(event.target) //passar possível mudança de altura aqui
+    smoothScrollTo(0, distanceFromTheTop, 1600);
+}
+
+menuLinks.forEach((link)=>{
+    link.addEventListener("click", scrollToSection);
+});
+
+function smoothScrollTo(endX, endY, duration) {
+  const startX = window.scrollX || window.pageXOffset;
+  const startY = window.scrollY || window.pageYOffset;
+  const distanceX = endX - startX;
+  const distanceY = endY - startY;
+  const startTime = new Date().getTime();
+
+  duration = typeof duration !== "undefined" ? duration : 400;
+
+  const easeInOutQuart = (time, from, distance, duration) => {
+    if ((time /= duration / 2) < 1)
+      return (distance / 2) * time * time * time * time + from;
+    return (-distance / 2) * ((time -= 2) * time * time * time - 2) + from;
+  };
+
+  const timer = setInterval(() => {
+    const time = new Date().getTime() - startTime;
+    const newX = easeInOutQuart(time, startX, distanceX, duration);
+    const newY = easeInOutQuart(time, startY, distanceY, duration);
+    if (time >= duration) {
+      clearInterval(timer);
+    }
+    window.scroll(newX, newY);
+  }, 1000 / 60);
+}
+
+
+
+
+// Função para alternar a classe ativa
+function highlightSection() {
+    const sections = document.querySelectorAll("section");
+    const navLinks = document.querySelectorAll(".ativado");
+  
+    let currentSection = "";
+  
+    sections.forEach((section) => {
+      const sectionTop = section.offsetTop;
+      const sectionHeight = section.clientHeight;
+      if (window.scrollY >= sectionTop - sectionHeight / 3) {
+        currentSection = section.getAttribute("id");
+      }
+    });
+  
+    navLinks.forEach((link) => {
+      link.classList.remove("active"); // Remove 'active' de todos os links
+      if (link.getAttribute("href") === `#${currentSection}`) {
+        link.classList.add("active"); // Adiciona 'active' ao link atual
+      }
+    });
+  }
+  
+  // Adiciona o evento de scroll
+  window.addEventListener("scroll", highlightSection);
+  
